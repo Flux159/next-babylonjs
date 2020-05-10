c37517703d0dc4d4f00af2ec7d0508dd11bc4195
Flux159 <flux159@gmail.com>
2020-05-09 23:42:26 -0400

    Initial rewrite of index.tsx and adding typescript.

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,51 +13,46 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        <p>with <a href="https://www.typescriptlang.org/">Typescript</a> and <a href="https://github.com/brianzinn/react-babylonjs">react-babylonjs</a>!</p>
+
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by editing <code>pages/index.tsx</code><br /> 
+          For a full screen view, see <code>pages/fullscreen.tsx</code>
+        </p>
+
+        <p className="description">
         </p>
 
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
             <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <p>Find information about Next.js</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className="card">
+          <a href="https://www.babylonjs.com/" className="card">
             <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <p>Learn about Babylonjs!</p>
           </a>
 
           <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
+            href="https://brianzinn.github.io/react-babylonjs/?path=/story/babylon-basic--default-playground"
             className="card"
           >
             <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <p>Discover react-babylonjs examples.</p>
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://nextjs.org/docs/deployment#static-html-export"
             className="card"
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Build &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Quickly build static websites.
             </p>
           </a>
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
 
       <style jsx>{`
         .container {
@@ -103,6 +98,11 @@ export default function Home() {
         }
 
         .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+
+        p a {
           color: #0070f3;
           text-decoration: none;
         }
